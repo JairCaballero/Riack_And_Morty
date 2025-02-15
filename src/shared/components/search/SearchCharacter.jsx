e16c302978e0ce346868f89c50d8f4126cf9ae12
JairCaballero <dwancaballero@gmail.com>
2025-02-14 22:27:19 -0500

    chore: Valida el input de busqqueda contra espacios extra o tabulaciones

--- src/shared/components/search/SearchCharacter.jsx
+++ src/shared/components/search/SearchCharacter.jsx
@@ -1,4 +1,4 @@
-import { useContext, useRef } from "react"
+import { useContext, useRef, useState } from "react"
 import { useLocation, useNavigate } from "react-router-dom"
 import { FiltersContext } from "@/modules/characters/context/Filters"
 import { PublicRoutes } from "@/router/routes"
@@ -8,27 +8,43 @@ const SearchCharacter = () => {
   const location = useLocation()
   const navigate = useNavigate()
   const debounceRef = useRef(null)
+  const [inputValue, setInputValue] = useState('')
   const { onSearch } = useContext(FiltersContext)
 
   const handleChange = (e) => {
-    const value = e.target.value
+    let value = e.target.value
+
+     // 1️. Evitar tabulaciones
+     if (/\t/.test(value)) {
+      return // No actualiza el estado si el usuario presiona "Tab"
+    }
+
+    // 2️. Evitar más de un espacio seguido antes de setear el estado
+    if (/\s{2,}/.test(value)) {
+      return // No actualiza el estado si hay dos espacios seguidos
+    }
+
+    setInputValue(value) // Guarda el valor solo si es válido
 
     if (debounceRef.current) clearTimeout(debounceRef.current)
     debounceRef.current = setTimeout(() => {
       if (location.pathname !== PublicRoutes.character) {
         navigate(PublicRoutes.character)
       }
-      onSearch({ name: value.trim() })
+
+      onSearch({ name: value })
     }, 700)
   }
 
   return (
     <input
-      onChange={(event) => handleChange(event)}
+      onChange={handleChange}
+      value={inputValue}
       type="text"
-      placeholder='Rick Sanchez...'
+      placeholder="Rick Sanchez..."
       className={styles.search_input}
     />
   )
 }
+
 export default SearchCharacter
